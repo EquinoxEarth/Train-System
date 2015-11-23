90dd8de50346ada6dc38bb94c883833229a9a015
Cole Robert Finnie <colefinnie@hotmail.com>
2015-11-23 12:40:09 -0500

    Initial Templating

---
+++ TrainSystem/TrainSystem/Classes/FileManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace TrainSystem
+{
+    class FileManager
+    {
+        /// <summary>
+        /// Loads a file's records into memory
+        /// </summary>
+        /// <param name="fileName">Name of file to be loaded</param>
+        /// <returns>Whether or not the file loaded</returns>
+        public bool Load(string fileName)
+        {
+
+            return true;
+        }
+
+        public bool Save(string fileName)
+        {
+
+            return true;
+        }
+    }
+}
---
+++ TrainSystem/TrainSystem/Classes/TrainSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainSystem.Classes
+{
+    /// <summary>
+    /// Contains all runtime variables and commands used in the Train Management System
+    /// </summary>
+    class TrainSystem
+    {
+    }
+}
