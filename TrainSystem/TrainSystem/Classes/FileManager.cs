4329805acad26228fe828a4a4bdbdac14bb2be13
Cole Robert Finnie <colefinnie@hotmail.com>
2015-11-23 14:35:27 -0500

    Wrote the Load method

--- TrainSystem/TrainSystem/Classes/FileManager.cs
+++ TrainSystem/TrainSystem/Classes/FileManager.cs
@@ -10,6 +10,8 @@ namespace TrainSystem.Classes
 {
     class FileManager
     {
+        // SCHEMA FOR FILE: Schedule, Station, Train, Direction, Time //
+
         private TSystem trainSystem;
 
         /// <summary>
@@ -27,8 +29,48 @@ namespace TrainSystem.Classes
         /// <returns>Whether or not the file loaded</returns>
         public bool Load(string fileName)
         {
+            // Create Empty Reader (For Scope reasons) //
+            StreamReader sr = null;
 
-            return true;
+            // Try Catch to find any file errors
+            try {
+                // Create the Reader //
+                sr = new StreamReader("./" + fileName + ".csv");
+
+                // Move cursor to first Record //
+                string record = sr.ReadLine();
+
+                // Loop through Records //
+                while (record != null)
+                {
+                    // Split the Record into Fields //
+                    string[] fields = record.Split(',');
+
+                    // Add Record to TrainSystem
+                    trainSystem.AddRecord(fields[0], fields[1], fields[2], fields[3], Convert.ToInt32(fields[4]));
+
+                    // Grab the next Record //
+                    record = sr.ReadLine();
+                }
+
+                return true;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.Write(ex.Message);
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+
+                return false;
+            }
+            finally
+            {
+                sr.Close();
+            }
         }
 
         public bool Save(string fileName)
